6c0303b7d53e58457b1cb23d2d6c3648409f5965
Martin Unterreiner <martin.unterreiner@posteo.de>
2020-05-04 16:21:08 +0200

    Added Aufgabe 1.8

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,30 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include <iostream>
+#include <utility>
+#include <cmath>
 
 int main(int argc, char* argv[])
 {
   return Catch::Session().run(argc, argv);
+}
+
+int gcd(int a, int b){
+  // std::cout << "Aufgabe 1.8 \n";
+  if(b == 0){
+    return a;
+  } else if(a == 0){
+    return b;
+  } else if(a > b){
+    return gcd(a - b, b);
+  } else {
+    return gcd(a, b - a);
+  }
+}
+
+TEST_CASE ( " describe_gcd " , " [gcd] " )
+{
+  REQUIRE ( gcd (2 ,4) == 2);
+  REQUIRE ( gcd (9 ,6) == 3);
+  REQUIRE ( gcd (3 ,7) == 1);
 }
