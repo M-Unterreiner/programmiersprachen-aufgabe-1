04cc15c3878bf6297625bc7a8546abb7176d7014
Martin Unterreiner <martin.unterreiner@posteo.de>
2020-05-04 16:53:37 +0200

    Added Aufgabe 1.9

--- source/tests.cpp
+++ source/tests.cpp
@@ -28,3 +28,29 @@ TEST_CASE ( " describe_gcd " , " [gcd] " )
   REQUIRE ( gcd (9 ,6) == 3);
   REQUIRE ( gcd (3 ,7) == 1);
 }
+
+int checksum(int check){
+
+  int sum = 0;
+  if (check <=0){
+    return 0;
+  } else if (check < 10){
+    sum = check;
+    
+  } else {
+    while (check != 0){
+      sum += check % 10;
+      check /= 10;
+    }
+  }
+  return sum;
+}
+
+TEST_CASE ( " checksum " , " [checksum] " )
+{
+  REQUIRE ( checksum (0) == 0);
+  REQUIRE ( checksum (33) == 6);
+  REQUIRE ( checksum (11998842) == 42);
+}
+
+
